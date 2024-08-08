b8dbb028a85a8585a734718f5106190c293e0e76
Brian Kelley <bmkelle@sandia.gov>
2024-08-08 14:38:49 -0600

    Register pipelines in lapis-opt, not just passes

--- mlir/tools/lapis-opt/lapis-opt.cpp
+++ mlir/tools/lapis-opt/lapis-opt.cpp
@@ -1,18 +1,21 @@
 //===- lapis-opt.cpp - LAPIS pass Driver -------------------------===//
 //
-//#include "mlir/InitAllDialects.h"
-//#include "mlir/InitAllExtensions.h"
-//#include "mlir/InitAllPasses.h"
 #include "mlir/Dialect/Kokkos/IR/KokkosDialect.h"
+#include "mlir/Dialect/Kokkos/Pipelines/Passes.h"
 #include "mlir/Dialect/Kokkos/Transforms/Passes.h"
+#include "mlir/Dialect/SparseTensor/Pipelines/Passes.h"
 #ifdef ENABLE_PART_TENSOR
 #include "mlir/Dialect/PartTensor/IR/PartTensor.h"
 #include "mlir/Dialect/PartTensor/Transforms/Passes.h"
+#include "mlir/Dialect/PartTensor/Pipelines/Passes.h"
 #endif
 #include "mlir/Support/LogicalResult.h"
 #include "mlir/Tools/mlir-opt/MlirOptMain.h"
 
 using namespace mlir;
+
+void registerLAPISPipelines() {
+}
 
 int main(int argc, char **argv) {
   // lapis-opt is intended to drive only passes from the custom
@@ -24,10 +27,16 @@ int main(int argc, char **argv) {
     mlir::part_tensor::PartTensorDialect
   >();
 
-  mlir::registerKokkosPasses();
+  // Register LAPIS pipelines and passes
+  sparse_tensor::registerSparseTensorPipelines();
+
 #ifdef ENABLE_PART_TENSOR
+  part_tensor::registerPartTensorPipelines();
   mlir::registerPartTensorPasses();
 #endif
+
+  kokkos::registerKokkosPipelines();
+  mlir::registerKokkosPasses();
 
   return mlir::asMainReturnCode(mlir::MlirOptMain(
       argc, argv, "LAPIS/MLIR pass driver\n", registry));
