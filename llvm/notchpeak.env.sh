67eaa8093d6923bc72876672a1cba1b2b2cfcf95
Miheer Vaidya <m.vaidya@utah.edu>
2024-08-09 13:33:34 -0600

    remove some PID

--- llvm/notchpeak.env.sh
+++ llvm/notchpeak.env.sh
@@ -1,11 +1,14 @@
-export MODULEPATH=$MODULEPATH:/scratch/general/vast/u1290058/setup/spack/share/spack/lmod/linux-rocky8-x86_64/gcc/11.2.0
 module load cmake/3.26.0.lua
-module load gcc/13.2.0-ycht4e6
+module load gcc/13.1.0
 module load ninja/1.11.1.lua
 module load ccache/4.6.1.lua
-module load mold/2.1.0-jxbrdya.lua
+which mold &> /dev/null || module load mold/2.1.0
+. ~/scratchVast/setup/spack/share/spack/setup-env.sh
+cd ..
+spack env activate .
+spack install
 export CC=`which gcc`
 export CXX=`which g++`
-export CCACHE_DIR=/scratch/general/vast/u1290058/mlirWorkspace/ccache
+export CCACHE_DIR=`readlink -f ~/scratchVast/mlirWorkspace/ccache`
 export PATH=`git rev-parse --show-toplevel`/llvm/build/bin:$PATH
 export MLIR_Workspace=$(readlink -f ${0:a:h}/../..)
