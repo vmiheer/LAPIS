922f4769babbd400eda25a0152897b1773bd7a75
Miheer Vaidya <m.vaidya@utah.edu>
2024-10-28 15:32:00 -0600

    🔧 script updates for notchpeak

--- scripts/notchpeak.env.sh
+++ scripts/notchpeak.env.sh
@@ -1,7 +1,17 @@
+#!/uufs/chpc.utah.edu/common/home/u1290058/.local/bin/zsh
+#
+module () {
+  eval $($LMOD_CMD zsh "$@") && eval $(${LMOD_SETTARG_CMD:-:} -s sh)
+}
+
+export ON_NOTCHPEAK=1
 module load cmake/3.26.0.lua
-module load gcc/13.1.0
 module load ninja/1.11.1.lua
 module load ccache/4.6.1.lua
+module load cuda/12.2
+# reloading gcc causes error about loading zlib module
+which gcc | grep gcc-11.2.0 &> /dev/null || module load gcc/11.2.0
+module load openmpi/4.1.6
 which mold &> /dev/null || module load mold/2.1.0
 . ~/scratchVast/setup/spack/share/spack/setup-env.sh
 cd ..
@@ -13,4 +23,8 @@ export CC=`which gcc`
 export CXX=`which g++`
 export CCACHE_DIR=`readlink -f ~/scratchVast/mlirWorkspace/ccache`
 export PATH=`git rev-parse --show-toplevel`/llvm/build/bin:$PATH
-export MLIR_Workspace=$(readlink -f ${0:a:h}/..)
+export WORKSPACE=$(readlink -f ${0:a:h}/..)
+
+cd $WORKSPACE
+source $WORKSPACE/LAPIS/llvm/setup_workspace.sh
+source $WORKSPACE/LAPIS/llvm/build_workspace.sh
