da299adc1d1849681c658ab4282e9f4f3a9a71fc
Miheer Vaidya <m.vaidya@utah.edu>
2024-12-20 10:53:09 -0700

    🔧 fix script path

--- scripts/ubuntu.env.sh
+++ scripts/ubuntu.env.sh
@@ -8,7 +8,8 @@ declare -f spack_setup &> /dev/null && spack_setup
 cd ..
 spack env activate .
 which python | grep ".spack-env/view" 2>&1 > /dev/null || spack install
-python -c "import numpy" 2>&1 || pip install -r llvm/notchpeak.requirements.txt
+python -c "import numpy" 2>&1 || \
+  pip install -r scripts/notchpeak.requirements.txt
 
 export CC=`which gcc-12`
 export CXX=`which g++-12`
