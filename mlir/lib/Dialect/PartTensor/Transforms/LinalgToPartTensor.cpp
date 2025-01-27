8208d3bac2300b1670bc3168679de5439aeb32cd
Miheer Vaidya <m.vaidya@utah.edu>
2025-01-27 11:11:36 -0700

    ✨[pt][linagToPT] empty body

--- mlir/lib/Dialect/PartTensor/Transforms/LinalgToPartTensor.cpp
+++ mlir/lib/Dialect/PartTensor/Transforms/LinalgToPartTensor.cpp
@@ -35,28 +35,89 @@ struct LinalgToPartTensorPass
   LinalgToPartTensorPass() = default;
   LinalgToPartTensorPass(const LinalgToPartTensorPass &pass) = default;
 
+  LogicalResult processFunction(func::FuncOp funcOp,
+                                ImplicitLocOpBuilder &builder) {
+    using llvm::dbgs;
+    auto &region = funcOp.getRegion();
+    if (!region.hasOneBlock())
+      return failure();
+    // if there is any sub-region which is not a linalg op, disable conversion
+    size_t numLinalgOps = 0;
+    optional<LinalgOp> linalgOp;
+    auto walkResult = region.walk([&](Operation *op) {
+      if (!isa<LinalgOp>(op) && op->getRegions().size() > 0) {
+        dbgs() << fmt::format("Found region so disabling conversion");
+        dbgs() << *op;
+        return WalkResult::interrupt();
+      } else if (isa<LinalgOp>(op)) {
+        linalgOp = cast<LinalgOp>(op);
+        numLinalgOps++;
+      }
+      return WalkResult::advance();
+    });
+    if (walkResult.wasInterrupted()) {
+      fmt::println("walk interrupted");
+      return failure();
+    }
+    if (numLinalgOps != 1) {
+      fmt::println("Found {} linalg ops so disabling conversion", numLinalgOps);
+      return (numLinalgOps == 0) ? success() : failure();
+    }
+
+    const bool AllSparse = lapis::part_tensor::hasAllSparseResult(*linalgOp) &&
+                           lapis::part_tensor::hasAllSparseOperands(*linalgOp);
+    if (!AllSparse) {
+      fmt::println("Only supported when all arguments and results are sparse!");
+      return failure();
+    }
+
+    auto partTensorTypes = llvm::to_vector(
+        llvm::map_range(linalgOp->getOperation()->getOperands().getTypes(),
+                        [&](Type t) -> Type {
+                          return lapis::part_tensor::getPartTensorType(
+                              builder.getContext(), cast<RankedTensorType>(t));
+                        }));
+    auto opFunctionTy =
+        FunctionType::get(builder.getContext(), partTensorTypes, {});
+    auto opFunc =
+        builder.create<func::FuncOp>(funcOp.getLoc(), "dist_op", opFunctionTy);
+    opFunc.setPrivate();
+    Block *entryBB = opFunc.addEntryBlock();
+    builder.setInsertionPointToEnd(entryBB);
+    builder.create<func::ReturnOp>(funcOp.getLoc());
+    return success();
+  }
   void runOnOperation() override {
-    auto *ctx = &getContext();
-    RewritePatternSet patterns(ctx);
-    PartTensorTypeToPtrConverter converter;
-    ConversionTarget target(*ctx);
-    // Allow func.call
-    // target.addDynamicallyLegalOp<func::FuncOp>([&](func::FuncOp op) {
-    //   return converter.isSignatureLegal(op.getFunctionType());
-    // });
-    // target.addDynamicallyLegalOp<func::CallOp>([&](func::CallOp op) {
-    //   return converter.isSignatureLegal(op.getCalleeType());
-    // });
-    // target.addLegalDialect<
-    //     arith::ArithDialect, bufferization::BufferizationDialect,
-    //     LLVM::LLVMDialect, memref::MemRefDialect, scf::SCFDialect,
-    //     sparse_tensor::SparseTensorDialect>();
-    // target.addLegalOp<UnrealizedConversionCastOp>();
-    // Populate with rules and apply rewriting rules.
-    populateLinalgToPartTensorPatterns(converter, patterns);
-    if (failed(applyPartialConversion(getOperation(), target,
-                                      std::move(patterns))))
-      signalPassFailure();
+    // auto *ctx = &getContext();
+    ModuleOp module = getOperation();
+    Location loc = module->getLoc();
+    ImplicitLocOpBuilder builder =
+        ImplicitLocOpBuilder::atBlockEnd(loc, module.getBody());
+    module.walk([&](func::FuncOp funcOp) {
+      LogicalResult res = processFunction(funcOp, builder);
+      (void)res;
+    });
+
+    // RewritePatternSet patterns(ctx);
+    // PartTensorTypeToPtrConverter converter;
+    // ConversionTarget target(*ctx);
+    // // Allow func.call
+    // // target.addDynamicallyLegalOp<func::FuncOp>([&](func::FuncOp op) {
+    // //   return converter.isSignatureLegal(op.getFunctionType());
+    // // });
+    // // target.addDynamicallyLegalOp<func::CallOp>([&](func::CallOp op) {
+    // //   return converter.isSignatureLegal(op.getCalleeType());
+    // // });
+    // // target.addLegalDialect<
+    // //     arith::ArithDialect, bufferization::BufferizationDialect,
+    // //     LLVM::LLVMDialect, memref::MemRefDialect, scf::SCFDialect,
+    // //     sparse_tensor::SparseTensorDialect>();
+    // // target.addLegalOp<UnrealizedConversionCastOp>();
+    // // Populate with rules and apply rewriting rules.
+    // populateLinalgToPartTensorPatterns(converter, patterns);
+    // if (failed(applyPartialConversion(getOperation(), target,
+    //                                   std::move(patterns))))
+    //   signalPassFailure();
   }
 };
 } // namespace
