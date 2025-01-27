5a6552012e457cd16276b4d245947db8c97ece9b
Miheer Vaidya <m.vaidya@utah.edu>
2025-01-27 11:11:36 -0700

    ✨[pt][linagToPT] empty body

--- mlir/include/lapis/Dialect/PartTensor/Transforms/LinalgToPartTensor.h
+++ mlir/include/lapis/Dialect/PartTensor/Transforms/LinalgToPartTensor.h
@@ -6,9 +6,21 @@
 namespace lapis {
 namespace part_tensor {
 
-using mlir::TypeRange;
 using mlir::Operation;
+using mlir::RankedTensorType;
 using mlir::Type;
+using mlir::TypeRange;
+using mlir::sparse_tensor::SparseTensorEncodingAttr;
+
+Type getPartTensorType(mlir::MLIRContext *context,
+                       const RankedTensorType &spTensorTy) {
+  using mlir::part_tensor::PartTensorEncodingAttr;
+  SparseTensorEncodingAttr encoding;
+  auto ptEncoding = PartTensorEncodingAttr::get(
+      context, 1, mlir::sparse_tensor::getSparseTensorEncoding(spTensorTy));
+  return mlir::RankedTensorType::get(spTensorTy.getShape(),
+                                     spTensorTy.getElementType(), ptEncoding);
+}
 
 // See the any version already defined in sparse_tensor
 inline bool hasAllSparseType(TypeRange types) {
